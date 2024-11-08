9557f76419c0c7acfc4c2c2de0bc89ba08c5c380
Emre Colakoglu <emrinho@gmail.com>
2024-11-08 14:04:20 +0300

    added dockerfile

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,8 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
+  output: "standalone",
+  eslint: { ignoreDuringBuilds: true },
 };
 
 export default nextConfig;
