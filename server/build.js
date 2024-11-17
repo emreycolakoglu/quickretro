6f502b2499b1daa1a40a26dfbda6309855050693
Emre Colakoglu <emrinho@gmail.com>
2024-11-17 17:30:43 +0300

    build fix

--- server/build.js
+++ server/build.js
@@ -2,11 +2,11 @@ const esbuild = require("esbuild");
 
 esbuild
   .build({
-    entryPoints: ["src/server.ts"], // Replace with your main TypeScript file
+    entryPoints: ["src/main.ts"], // Replace with your main TypeScript file
     bundle: true, // Bundle dependencies into a single file
     platform: "node", // Target Node.js environment
     target: "node20", // Specify your Node.js version
-    outfile: "../release/server.js", // Output file
+    outfile: "../release/main.js", // Output file
     loader: { ".ts": "ts" }, // Add support for TypeScript
     sourcemap: true, // Optional: Generate source maps
     minify: false, // Optional: Minify the output for production
