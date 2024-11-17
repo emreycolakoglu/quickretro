befbadc7fd469c76193a911f20468ced3ead8bc2
Emre Colakoglu <emrinho@gmail.com>
2024-11-17 17:43:59 +0300

    added root layout

--- client/src/routes/__root.tsx
+++ client/src/routes/__root.tsx
@@ -1,20 +1,43 @@
+import { Button } from "@/components/ui/button";
 import { createRootRoute, Link, Outlet } from "@tanstack/react-router";
 import { TanStackRouterDevtools } from "@tanstack/router-devtools";
 
 export const Route = createRootRoute({
   component: () => (
-    <>
-      <div className="p-2 flex gap-2">
-        <Link to="/" className="[&.active]:font-bold">
-          Home
-        </Link>{" "}
-        <Link to="/about" className="[&.active]:font-bold">
-          About
-        </Link>
-      </div>
-      <hr />
-      <Outlet />
+    <div className="min-h-screen flex flex-col">
+      <header className="p-4 flex items-center justify-between">
+        <div className="flex items-center space-x-2">logo</div>
+        <nav>
+          <Button variant="ghost" asChild>
+            <Link to="/" className="[&.active]:font-bold">
+              Home
+            </Link>
+          </Button>
+          <Button variant="ghost" asChild>
+            <Link to="/about" className="[&.active]:font-bold">
+              About
+            </Link>
+          </Button>
+        </nav>
+      </header>
+
+      <main className="flex-grow flex items-center justify-center px-4">
+        <Outlet />
+      </main>
+
+      <footer className="p-4 text-center text-sm text-gray-600 dark:text-gray-400">
+        <p>&copy; 2024 RetroMeet. All rights reserved.</p>
+        <div className="mt-2">
+          <a href="#" className="hover:underline">
+            Terms of Service
+          </a>
+          {" • "}
+          <a href="#" className="hover:underline">
+            Privacy Policy
+          </a>
+        </div>
+      </footer>
       <TanStackRouterDevtools />
-    </>
+    </div>
   ),
 });
