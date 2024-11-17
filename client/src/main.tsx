80fcace28cd5d14e9612570ec0635d92cd29b599
Emre Colakoglu <emrinho@gmail.com>
2024-11-17 12:53:59 +0300

    added tanstack query

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,13 +1,18 @@
 import React, { StrictMode } from "react";
 import ReactDOM from "react-dom/client";
 import { RouterProvider, createRouter } from "@tanstack/react-router";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import "./index.css";
 
 // Import the generated route tree
 import { routeTree } from "./routeTree.gen";
 
+// Create a query client
+const queryClient = new QueryClient();
+
 // Create a new router instance
-const router = createRouter({ routeTree });
+const router = createRouter({ routeTree } as any);
 
 // Register the router instance for type safety
 declare module "@tanstack/react-router" {
@@ -22,7 +27,10 @@ if (!rootElement.innerHTML) {
   const root = ReactDOM.createRoot(rootElement);
   root.render(
     <StrictMode>
-      <RouterProvider router={router} />
+      <QueryClientProvider client={queryClient}>
+        <RouterProvider router={router} />
+        <ReactQueryDevtools initialIsOpen={false} />
+      </QueryClientProvider>
     </StrictMode>
   );
 }
